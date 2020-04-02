355f9a8d21433fd0ca58a41cb7ba8503d61f498d
EdiOancea <oancea.eduard65@gmail.com>
2020-04-02 23:26:13 +0300

    Rewrite some types.

--- front/src/containers/Dashboard/index.tsx
+++ front/src/containers/Dashboard/index.tsx
@@ -1,24 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import dayjs from 'dayjs';
 
 import APIRequests from '../APIRequests';
 import DashboardComponent from '../../components/Dashboard';
-
-const createData = (
-  name: string,
-  quantity: number,
-  calories: number,
-  purchaseDate: Date,
-  expirationDate: Date,
-  consumptionDate: Date,
-): GroceryListItem => ({
-  name,
-  quantity,
-  calories,
-  purchaseDate: dayjs(purchaseDate).format('DD/MM/YYYY'),
-  expirationDate: dayjs(expirationDate).format('DD/MM/YYYY'),
-  consumptionDate: dayjs(expirationDate).format('DD/MM/YYYY'),
-});
 
 const Dashboard: React.FC<{}> = () => {
   const [items, setItems] = useState<GroceryListItem[]>([]);
